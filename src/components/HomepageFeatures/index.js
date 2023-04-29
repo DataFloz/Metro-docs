44e03e818bfbde28adc73c6e8960ff608605df95
ofirelarat <96ofir11@gmail.com>
2023-04-29 23:10:37 +0300

    init docusaurus

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Infrastructure as code',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Infrastructure as code for easy and efficient infrastructure management.
+      </>
+    ),
+  },
+  {
+    title: 'Real-time stream processing',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Strong capabilities for real-time stream processing. With Chain of responsibility design for improved reliability and reduced downtime.
+      </>
+    ),
+  },
+  {
+    title: 'Multi-cloud support',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Multi-cloud and Flexible deployment options, for increased choice and flexibility in infrastructure deployment.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
