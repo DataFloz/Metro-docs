894c37b4280a1b82dd6242187405610b58016466
ofirelarat <96ofir11@gmail.com>
2023-05-11 22:26:12 +0300

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,14 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'DataFloz', // Usually your GitHub org/user name.
   projectName: 'Metro-docs', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
